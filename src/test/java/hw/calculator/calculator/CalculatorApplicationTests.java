33d1122e0468d99f419538dc071a783fccccfa79
yanyichi <yanyichi143@hotmail.com>
2019-10-29 15:17:46 -0700

    add more testcase

--- src/test/java/hw/calculator/calculator/CalculatorApplicationTests.java
+++ src/test/java/hw/calculator/calculator/CalculatorApplicationTests.java
@@ -16,18 +16,42 @@ class CalculatorApplicationTests {
 	public void testAdd() {
 		assertEquals(5, operator.add(2,3));
 	}
+
+	@Test
+	public void testNegAdd() {
+		assertEquals(-5, operator.add(-2,-3));
+	}
+
 	@Test
 	public void testSub() {
 		assertEquals(1, operator.sub(3,2));
 	}
+
+	@Test
+	public void testNegSub() {
+		assertEquals(-2, operator.sub(3,5));
+	}
+
 	@Test
 	public void testMul() {
 		assertEquals(6, operator.mul(2,3));
 	}
+
+	@Test
+	public void testFractionMul() {
+		assertEquals(7.5, operator.mul(2.5,3));
+	}
+
 	@Test
 	public void testDiv() {
 		assertEquals(2, operator.div(6,3));
 	}
+
+	@Test
+	public void testFractionDiv() {
+		assertEquals(3.5, operator.div(7,2));
+	}
+
 	@Test
 	public void testMod() {
 		assertEquals(5, operator.mod(23,6));
